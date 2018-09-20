f25d087401e0de5dcaccee46be365e0d84c98500
jovany <cap.vany@gmail.com>
2018-09-20 13:16:49 -0700

    Setting up basic file opening, reading, and exception catching

--- src/wc.java
+++ src/wc.java
@@ -1,6 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class wc {
     public static void main(String [] args){
         System.out.println("Hey hm");
+        String fileName="temp.txt";
+        String line=null;
+        try{
+
+
+            FileReader reading= new FileReader(fileName);
+            BufferedReader buff= new BufferedReader(reading);
+            while((line=buff.readLine()) !=null){
+                System.out.println(line);
+            }
+             buff.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Cannot open file");
+        }
+        catch(IOException e){
+            System.out.println("error reading file");
+        }
 
     }
 }
